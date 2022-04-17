709880447fcc38534481a5b00a586db15a42ce63
KhalidRupam <42520040+KhalidRupam@users.noreply.github.com>
2022-04-18 00:52:35 +0600

    Khalid Controller added

---
+++ GitAnalysis/Controllers/KhalidController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitAnalysis.Controllers
+{
+    public class KhalidController : Controller
+    {
+        // GET: Khalid
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
