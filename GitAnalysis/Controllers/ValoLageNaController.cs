3e3b7d5f535a03156d027d82efd7b3ee73f72b2d
Zahid Sheikh <48240960+XodeXiphoidXahid@users.noreply.github.com>
2022-04-18 01:14:13 +0600

    dddd

---
+++ GitAnalysis/Controllers/ValoLageNaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitAnalysis.Controllers
+{
+    public class ValoLageNaController : Controller
+    {
+        // GET: ValoLageNa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
